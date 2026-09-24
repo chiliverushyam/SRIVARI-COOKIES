8c691fd1dee4ddc9941591dd7c1e065053b3d658
chiliverushyam <74700798+chiliverushyam@users.noreply.github.com>
2026-09-24 18:44:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.SRIVARI_CONFIG={
 
   // Paste ONLY your Razorpay Key ID here.
   // Never put the Razorpay Key Secret in this file.
-  RAZORPAY_KEY_ID:"rzp_live_TfRVZRNY3dWwWF,IjP8M2JogcdQbWIUZAhpbzJY",
+  RAZORPAY_KEY_ID:"rzp_live_Tft03Hb9Z3Mdwt",
 
   RAZORPAY_FUNCTION_URL:"https://szsqbqcodtlruorcsgpq.supabase.co/functions/v1/create-razorpay-order",
 
